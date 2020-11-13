ad5720bd2180a3ad57ac125ef212bef537f9b0eb
Marshall Bertschy <basemarsh@gmail.com>
2020-11-13 11:44:09 +0700

    added function that gives correct css colors to text area depending on time

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
     $("#currentDay").text(moment().format('dddd MMM Do'));
-    var time = moment().format('h a');
-    var timeBefore = ["12 am","1 am","2 am","3 am","4 am","5 am","6 am","7 am","8 am"]
-    var textarea = !("textarea")
+    var time = moment().hours()
+    var textarea = $("textarea")
     console.log(time)
-    if (time !== timeBefore){
-        $(textarea).attr('class', 'col-md-10 description past')
+    function backgroundColor() {
+    $(".row").each(function() {
+    rowHour = parseInt($(this).attr("id"))
+    console.log(rowHour)
+    if (rowHour < time) {
+    $(this).addClass("past")
     }
+    else if(rowHour === time){
+    $(this).removeClass("past")    
+    $(this).addClass("present")
+    }
+    else {
+    $(this).removeClass("past") 
+    $(this).removeClass("present") 
+    $(this).addClass("future")
+    }
+}
+    )
+    }
+    backgroundColor()
 });
