0e63f5e047bb43359f56efaccb069e3b345184df
Marshall Bertschy <basemarsh@gmail.com>
2020-11-12 18:33:40 -0600

    trying to figure out how to get the text area css to work but class started

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-$("#currentDay").text(moment().format('dddd MMM Do'));
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#currentDay").text(moment().format('dddd MMM Do'));
+    var time = moment().format('h a');
+    var timeBefore = ["12 am","1 am","2 am","3 am","4 am","5 am","6 am","7 am","8 am"]
+    var textarea = !("textarea")
+    console.log(time)
+    if (time !== timeBefore){
+        $(textarea).attr('class', 'col-md-10 description past')
+    }
+});
