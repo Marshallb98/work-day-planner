15889aad2f7b418b92161f4ede56bab05a9ab43c
Marshall Bertschy <basemarsh@gmail.com>
2020-11-12 23:27:33 -0600

    tried getting local storage to run loop, not successful

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     console.log( "ready!" );
     $("#currentDay").text(moment().format('dddd MMM Do'));
     var time = moment().hours()
-    var textarea = $("textarea")
     console.log(time)
+    // function that changed the background color to gray red or green that corresponds with past present and future, time resets everyday at 12am to the new day
     function backgroundColor() {
     $(".row").each(function() {
     rowHour = parseInt($(this).attr("id"))
@@ -24,4 +24,15 @@ $(document).ready(function() {
     )
     }
     backgroundColor()
+
+    $("saveBtn").on("click", function(){
+        var input = $(this).siblings(".description").val();
+        var inputTime = $(this).parent().attr("id");
+    
+       localStorage.setItem(input, inputTime)
+    })
 });
+var localTime = [$("#9"),$("#10"),$("#11"),$("#12"),$("#13"),$("#14"),$("#15"),$("#16"),$("#17")]
+$(localTime).each(function() {
+    $(localTime).val(localStorage.getItem(localTime))
+})
